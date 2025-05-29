653e8180dd4f0d2fa9ada195ba5308413e3e2e3d
akinko0915 <114070658+akinko0915@users.noreply.github.com>
2025-05-29 12:13:22 +0900

    add reset button

--- number-gussing-game/script.js
+++ number-gussing-game/script.js
@@ -8,31 +8,38 @@ const guessSubmit = document.querySelector(".guessSubmit");
 const guessField = document.querySelector(".guessField");
 
 let guessCount = 0;
-let resetButton;
+const resetButton = document.querySelector(".resetButton");
 const result_element = document.createElement("p")
 
 function checkGuess(){
     guessCount++
-    guesses.innerHTML = guessCount;
-    lastResult.innerHTML = guessField.value;
-  
+    guesses.innerHTML = "Guessing Count: " + guessCount;
+    lastResult.innerHTML = "Previous guesses: " + guessField.value;
+
     if (guessField.value == randomNumber){
         result_element.textContent = "Correct!";
         guessSubmit.disabled = true
     } else {
         if (guessCount != 10) {
             if (guessField.value > randomNumber){
-                result_element.textContent = "higher"
+                result_element.textContent = "Last guess was too high"
                 lowOrHi.appendChild(result_element);
             } else {
-                result_element.textContent = "lower"
+                result_element.textContent = "Last guess was too low"
                 lowOrHi.appendChild(result_element);
             }
         } else {
             result_element.textContent = "Gameover!";
             guessSubmit.disabled = true
         }
-    }    
+    }
 }
 
 guessSubmit.addEventListener("click", checkGuess);
+resetButton.addEventListener("click", function() {
+    guesses.innerHTML = null
+    lastResult.innerHTML = null
+    result_element.textContent = "";
+    guessSubmit.disabled = false;
+    guessField.value = null
+})
